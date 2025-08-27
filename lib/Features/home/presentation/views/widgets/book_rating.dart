c0321f81f5c1b473b043f3f79b2edb68fdfe957e
OmarKhaled00e0 <ok4966541@gmail.com>
2025-08-27 18:44:23 +0300

    Book Details View Ui Done

--- lib/Features/home/presentation/views/widgets/book_rating.dart
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -1,24 +1,33 @@
-
 import 'package:awesome_icons/awesome_icons.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         // icon star
-        Icon(FontAwesomeIcons.solidStar , color: Color(0xffFFDD4F),),
+        Icon(FontAwesomeIcons.solidStar, size: 14, color: Color(0xffFFDD4F)),
         Gap(6),
         // rating
-        Text('4.8' , style: Styles.textStyle16,),
+        Text('4.8', style: Styles.textStyle16),
         Gap(5),
         // reviews
-        Text('(245)', style: Styles.textStyle14,),
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            '(245)',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+          ),
+        ),
       ],
     );
   }
--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,15 +1,15 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class CustomBookImage extends StatelessWidget {
+  const CustomBookImage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.sizeOf(context).height * .23,
       child: AspectRatio(
-        aspectRatio: 2.8 / 4,
+        aspectRatio: 2.6 / 4,
         child: Container(
           decoration: BoxDecoration(
             color: Colors.red,
