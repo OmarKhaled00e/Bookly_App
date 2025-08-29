9b5a12025e7cce5b0b658904bda2196330f0ccd3
OmarKhaled00e0 <ok4966541@gmail.com>
2025-08-29 19:42:35 +0300

    create featured_books method

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchNewstBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+}
---
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_book_state.dart
@@ -0,0 +1,24 @@
+part of 'featured_book_cubit.dart';
+
+abstract class FeaturedBookState extends Equatable {
+  const FeaturedBookState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeaturedBookInitial extends FeaturedBookState {}
+
+class FeaturedBookLoading extends FeaturedBookState {}
+
+class FeaturedBookFailure extends FeaturedBookState {
+  final String errMassage;
+
+  const FeaturedBookFailure(this.errMassage);
+}
+
+class FeaturedBookSuccess extends FeaturedBookState {
+  final List<BookModel> books;
+
+  const FeaturedBookSuccess(this.books);
+}
